3c51fda6dffdd1188818555e44a3bcd20b3c74dc
Paras Tyagi <parastyagi1105@gmail.com>
2025-07-16 17:16:44 +0530

    Fix CORS and update backend

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import app from './app.js';
-
+import cors from 'cors'
 dotenv.config();
-
+app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 mongoose
